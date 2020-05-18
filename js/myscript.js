e0c5aad61d4a042a58e00869f4faddec6ef4dec1
Giosuè Lentini <giosuelentini94@outlook.com>
2020-05-18 15:55:19 +0200

    modifica playlist con javascript

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+document.getElementById('heavy-metal').innerHTML = 'Heavy Metal';
+
+document.getElementById('pop').innerHTML = 'POP';
+
+document.getElementById('classic').innerHTML = 'Classic';
+
+document.getElementById('jazz').innerHTML = 'Jazz';
+
+document.getElementById('rock').innerHTML = 'Rock';
+
+document.getElementById('si-con-riso-ma-senza-lattosio').innerHTML = 'Si con riso ma senza lattosio';
+
+document.getElementById('lerba-del-vicino-e-sempre-piu-verde').innerHTML = 'L&apos;erba del vicino è sempre più verde';
